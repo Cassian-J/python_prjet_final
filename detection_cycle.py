0b7ce29b921a81c63f70d9114b0ed59420198e80
cassian.joly@ynov.com <cassian.joly@ynov.com>
2024-11-22 13:56:09 +0100

    end of the project

--- detection_cycle.py
+++ detection_cycle.py
@@ -3,11 +3,9 @@ class Detection_Cycle:
     def detection(historic_table):
         if historic_table != []:
             for i in range(len(historic_table)-1):
-                match_found = True
                 if len(historic_table[len(historic_table)-1]) == len(historic_table[i]):
                     for j in range(len(historic_table[i])):
                         if historic_table[len(historic_table)-1][j] != historic_table[i][j]:
-                            match_found = False
-                    if match_found:
-                        return i
+                            break
+                    return i
         return -1
