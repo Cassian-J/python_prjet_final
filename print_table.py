173b55a96353d165364cfed7f1a596a632b26d30
cassian.joly@ynov.com <cassian.joly@ynov.com>
2024-11-22 13:21:10 +0100

    nomenclature

--- print_table.py
+++ print_table.py
@@ -3,11 +3,11 @@ class PrintTable:
     @staticmethod
     def print(table):
         printing = ""
-        for col in range(len(table)):
-            for row in range(len(table[col])):
-                if table[col][row] == 0:
+        for row in range(len(table)):
+            for col in range(len(table[row])):
+                if table[row][col] == 0:
                     printing += "⬛"
-                elif table[col][row] == 1:
+                elif table[row][col] == 1:
                     printing += "⬜"
             printing += "\n"
         return printing
