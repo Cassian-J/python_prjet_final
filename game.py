3a1e6d83dc85ba3f25c6c960b4c5f6c18e97adb1
cassian joly <cassian.joly@ynov.com>
2024-11-25 14:47:47 +0100

    correction des erreur systeme sur windows

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-import os
 import time
 
 import generator_table
@@ -11,17 +10,17 @@ import key_controle
 
 class Game:
     def __init__(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
+        key_controle.Key_Controle.clear()
         while True:
             try:
                 self.col =int(input("Combien de colonnes et lignes voulez-vous dans cette partie ? (n x n)\n"))
-                os.system('cls' if os.name == 'nt' else 'clear')
+                key_controle.Key_Controle.clear()
                 if self.col < 3 or self.col >= 50:
                     print("Le nombre de colonnes et de lignes doivent être un entier positif, entre 3 et 49 compris.")
                 else:
                     break
             except ValueError:
-                os.system('cls' if os.name == 'nt' else 'clear')
+                key_controle.Key_Controle.clear()
                 print("Veuillez entrer un nombre entier.")
         self.row=self.col
         self.table = generator_table.Generator_Table.generate(self.col, self.row, [0, 1])
@@ -37,13 +36,13 @@ class Game:
             try:
                 print("[0] continuer la partie d'avant\n[1] commencer une nouvelle partie")
                 new_game = int(key_controle.Key_Controle.get_key().lower())
-                os.system('cls' if os.name == 'nt' else 'clear')
+                key_controle.Key_Controle.clear()
                 if new_game != 0 and new_game != 1:
                     print("Le nombre choisi doit être 0 ou 1.")
                 else:
                     break
             except ValueError:
-                os.system('cls' if os.name == 'nt' else 'clear')
+                key_controle.Key_Controle.clear()
                 print("Veuillez entrer 1 ou 0.")
 
         if new_game == 0:
@@ -63,14 +62,12 @@ class Game:
                 breakpoint
 
         while True:
-            os.system('cls' if os.name == 'nt' else 'clear')
+            key_controle.Key_Controle.clear()
             print("Ça fait", len(self.save_game.read_game_table(self.file)), "année que la population vit")
             print(print_table.Print_Table.print_game(self.table))
+
             debut_cycle = self.detection.detection(self.save_game.read_game_table(self.file))
             if debut_cycle != -1:
-                os.system('cls' if os.name == 'nt' else 'clear')
-                print("Ça fait", len(self.save_game.read_game_table(self.file)), "année que la population vit")
-                print(print_table.Print_Table.print_game(self.table))
                 print("Un cycle a été détecté. Il a commencé à la", debut_cycle, "année")
                 if self.automatic:
                     self.automatic = False
@@ -81,14 +78,15 @@ class Game:
                         print("[q] quittez\n[a] continuez automatiquement\n[c] continuez")
                         enter = key_controle.Key_Controle.get_key().lower()
                         if enter not in ['a', 'q', 'c']:
+                            key_controle.Key_Controle.clear()
                             print("Ça fait", len(self.save_game.read_game_table(self.file)), "année que la population vit")
                             print(print_table.Print_Table.print_game(self.table))
                             print("Choix invalide!")
                         else:
                             break
                     except ValueError:
-                        os.system('cls' if os.name == 'nt' else 'clear')
-                        print("Choix invalide!")
+                        key_controle.Key_Controle.clear()
+                        print("erreur du jeu")
             
                 if enter == 'a':
                     self.automatic = True
@@ -99,6 +97,7 @@ class Game:
                 print("Tapez sur [Entrée] pour arrété le mode automatique")
                 time.sleep(0.25)
                 if key_controle.Key_Controle.check_keypress():
+                    key_controle.Key_Controle.clear()
                     self.automatic = False
 
             self.table = update_table.Update_Table.update(self.table)
@@ -107,9 +106,9 @@ class Game:
 
     
     def end_game(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
+        key_controle.Key_Controle.clear()
         print("la partie est terminée")
         time.sleep(2.5)
-        os.system('cls' if os.name == 'nt' else 'clear')
+        key_controle.Key_Controle.clear()
         return
 
--- key_controle.py
+++ key_controle.py
@@ -1,18 +1,40 @@
+import os
 import sys
-import tty
-import termios
 import select
 
-class Key_Controle:
-    def check_keypress():
-        return select.select([sys.stdin], [], [], 0)[0] != []
+# Détecter le système d'exploitation pour choisir le module approprié
+if sys.platform == "win32":
+    import msvcrt
+else:
+    import tty
+    import termios
 
+class Key_Controle:
+    @staticmethod
+    def check_keypress():
+        Key_Controle.clear()
+        if sys.platform == "win32":
+            return msvcrt.kbhit()
+        else:
+            return select.select([sys.stdin], [], [], 0)[0] != []
+
+    @staticmethod
     def get_key():
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            key = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return key
+        if sys.platform == "win32":
+            return msvcrt.getch().decode('utf-8')
+        else:
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setraw(fd)
+                key = sys.stdin.read(1)
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            return key
+    
+    @staticmethod
+    def clear():
+        if sys.platform == "win32":
+            os.system('cls')
+        else:
+            os.system('clear')
